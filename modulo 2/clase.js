4fadb3eebf94ae8997c553eba2f2324bd9b19f60
dnielj5 <juniordanielpaulinoberroa@gmail.com>
2024-08-15 16:00:53 -0400

    Add files via upload

---
+++ modulo 2/clase.js
@@ -0,0 +1,35 @@
+// club platino
+// club vip
+
+// condicionales
+
+//membresia activa
+//vestimenta
+//edad
+
+let membresia = prompt ("cuenta con una membresia acctiva").toUpperCase();
+
+if (membresia === "SI"){
+    //bloque verdadero
+console.log ("excelente puedes pasr")
+}else{
+    //bloque falso
+    console.log ("no tienes tu membresia activa")
+}
+
+//Algun plato
+
+let platillo = prompt ("Dime tu plato dominicano favorito").toLocaleLowerCase();
+
+if (plato = "tostones con salami"){n
+    console.log (Tienes buen gusto)
+    } else if(plato === empanadas){
+    console.log("Muy buena eleccion")}
+    else if (plato === "dulce de leche"){        
+    }console.log("Unos de los mejores dulces")
+    else if(plato ==== salcocho){
+    }console.log("Un buen caldo 10/10")
+    else if (plato === lasaña){
+    }console.log("Buena compaña")
+    else if(plato === quesillo){
+    }console.log("Buen postre 10/10")
